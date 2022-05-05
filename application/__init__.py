d3165b6d912b398d49c3af35e01d90313a997996
Eric Greilich <egreilich4373@mail.dbu.edu>
2022-05-05 12:54:47 -0500

    clean up

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_mongoengine import MongoEngine 
 
 app = Flask(__name__)
-#app.config['MONGODB_SETTINGS'] = {'db': 'UTA_Enrollment'}
 app.config.from_object(Config)
 
 db = MongoEngine()
